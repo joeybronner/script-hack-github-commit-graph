f36b8a5ecda0fa58597f519ac5925fde5edf34f7
Joey Bronner <joeybronner@gmail.com>
2016-01-25 18:23:03 +0100

    Generate dates from calendar & section with code
    Remove unused values

--- README.md
+++ README.md
@@ -1,26 +1,19 @@
 ### How to
-Step 01: Create a new repo.  
-Step 02: Create an empty folder on your computer.  
+Step 01: Create a new repo.
+Step 02: Create an empty folder on your computer.
 Step 03: Add a new and empty dates.txt file to that folder
-Step 04: Add dates.sh to that folder and change the author name with your infos (Ex: --author="Joey Bronner <joeybronner@gmail.com>").  
 
-Website steps...  
+Website steps...
 
-Step 05: Open a terminal window and cd to your folder where you have dates.txt and dates.sh in it.  
-Step 06: Run git init to initialize the Git repo.  
-Step 07: Run ./dates.sh.  
-Step 08: Run git remote add origin git@github.com:your-username/your-project-name.git.  
-Step 09: Run git push --set-upstream origin master.  
-Step 10: Visit your user page to see the chart (Ex: http://github.com/your-username/your-project-name).  
+Step 05: Open a terminal window and cd to your folder where you have dates.txt and dates.sh in it.
+Step 06: Run git init to initialize the Git repo.
+Step 07: Run ./dates.sh.
+Step 08: Run git remote add origin git@github.com:your-username/your-project-name.git.
+Step 09: Run git push --set-upstream origin master.
+Step 10: Visit your user page to see the chart (Ex: http://github.com/your-username/your-project-name).
 
 ### Date format in dates.txt file
-Mon Aug 17  
-Tue Aug 18  
-Wed Aug 19  
-Thu Aug 20  
-Fri Aug 21 
+Day Mon DD YEAR
+Wed Dec 30 2015
 
-### How to change the year ?
-The commit year is setted in date.sh file, line 6.  
 
- 
--- web/js/welcome.js
+++ web/js/welcome.js
@@ -6,24 +6,51 @@
     // Create empty 365 day graph
     for (var i = 0, iLen = days.length; i < iLen; i++) {
       var tr = document.getElementById(days[i]);
-      var d = document.createElement('td');
+      var td = document.createElement('td');
       // Add <td> with first letter of the week day
-      d.setAttribute('class', 'day-of-wk');
-      d.textContent = days[i].substring(1, 0);
-      tr.appendChild(d);
+      td.setAttribute('class', 'day-of-wk');
+      td.textContent = days[i].substring(1, 0);
+      tr.appendChild(td);
       for (var j = 52; j > 0; j--) {
         var box = document.createElement('td');
         box.setAttribute('id', days[i] + '_day_' + j);
         box.classList.add('boxday');
         if (j === 1 && i > today) continue;
-          
-        box.onclick = function(event){
+
+        box.onclick = function(event) {
           var target = event.target;
           if (hasClass(target, 'green')) {
             target.classList.remove('green');
           } else {
             target.classList.add('green');
           }
+
+          // Code generation
+          var datesTxt = document.getElementById('dates-txt');
+          var calendar = document.getElementById('github_calendar');
+          var boxes = calendar.getElementsByClassName('boxday');
+          datesTxt.innerText = '';
+          console.log(datesTxt);
+          for (var i = 0, iLen = boxes.length; i < iLen; i++) {
+            if (hasClass(boxes[i], 'green')) {
+              var id = boxes[i].id;
+              var idSplited = id.split('_');
+              var day = idSplited[0];
+              var wk = idSplited[2];
+              var idDay = days.indexOf(day);
+
+              var dayDifference = (-today) + idDay;
+              var weekDifference = wk - 1;
+
+              // Calculate Date
+              var commitDate = new Date();
+              commitDate.setDate(d.getDate() + dayDifference - (weekDifference * 7));
+
+              var dateToString = commitDate.toString();
+              datesTxt.innerText += dateToString.substring(0,15) + String.fromCharCode(13);
+            }
+          }
+
         };
 
         tr.appendChild(box);
@@ -41,6 +68,15 @@ function resetGraph() {
 
 function hasClass(target, className) {
   return new RegExp('(\\s|^)' + className + '(\\s|$)').test(target.className);
+}
+
+function copyDates() {
+  //var datesTxt = document.getElementById('dates-txt');
+  copyToClipboard('dates-txt');
+}
+
+function copyToClipboard(elementId) {
+
 }
 
 window.onload = init;
